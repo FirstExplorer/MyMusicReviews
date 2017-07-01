5d2eb6c00b1f832b3f0265bcb11f20585081f40d
First Explorer <howemarkhowe@gmail.com>
2017-07-01 16:03:04 +0000

    rails commit from new laptop

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+  
+  def index
+    @reviews = Review.all
+  end
+  
+  def show
+    @review = Review.find(params[:id])
+  end
+  
+  def new
+    @review = Review.new
+  end
+  
+  def create
+    @review = Review.new
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
   root 'pages#home'
   
   get '/home', to: 'pages#home'
-
+  
+  #get '/reviews', to: 'reviews#index'
+  #get '/reviews/new', to: 'reviews#new', as: 'new_review'
+  #post '/reviews', to: 'reviews#create'
+  #get '/reviews/:id/edit', to: 'reviews#edit', as: 'edit_review'
+  #patch '/reviews/:id', to: 'review#update'
+  #get 'reviews/:id', to: 'reviews#show', as: 'review'
+  #delete '/reviews/:id', to: 'review#destroy'
+  
+  resources :reviews
 end
