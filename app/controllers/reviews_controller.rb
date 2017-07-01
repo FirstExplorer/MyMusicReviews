77c7ce7f65ddb960859b9ab63d42495fdc7155fd
First Explorer <howemarkhowe@gmail.com>
2017-07-01 16:55:13 +0000

    fixed forms

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,9 +13,37 @@ class ReviewsController < ApplicationController
   end
   
   def create
-    @review = Review.new
+    @review = Review.new(review_params)
+    @review.user = User.find(1)
+    
+    if @review.save
+      
+      flash[:success] = "Your review was created successfully"
+      redirect_to reviews_path 
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @review = Review.find(params[:id])
+  end
+  
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      
+      flash[:success] = "Your review was updated successfully"
+      redirect_to review_path(@review) 
+    else
+      render :edit
+    end
   end
 
-
+    private
+    
+    def review_params
+      params.require(:review).permit(:name, :title, :description)
+    end
 
 end
